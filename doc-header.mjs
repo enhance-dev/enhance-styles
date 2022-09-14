90743ef223c811a416d35bcbd2adb052cf7abd95
macdonst <simon.macdonald@gmail.com>
2022-09-14 14:09:58 -0400

    Issue #6: Add link to repo in generated css file
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ doc-header.mjs
@@ -0,0 +1,7 @@
+export default function DocHeader() {
+    return /*css*/`
+/**
+ * For more information please see the documentation at : https://github.com/enhance-dev/enhance-styles#readme
+ */
+`
+}
