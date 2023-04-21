1999160de0516e21f3c8ebb53a276bbecbbb5688
Cole Peters <cole@colepeters.com>
2023-04-21 15:14:05 -0500

    Update `sides`, margin, padding for logical properties

--- padding.mjs
+++ padding.mjs
@@ -5,15 +5,13 @@ export default function padding(state={}) {
   const { config, label:query } = state
   let output = /*css*/`
 /* PADDING */
-.p-none${query}{padding:0;}
-.py-none${query},
-.pt-none${query}{padding-top:0;}
-.px-none${query},
-.pr-none${query}{padding-right:0;}
-.py-none${query},
-.pb-none${query}{padding-bottom:0;}
-.px-none${query},
-.pl-none${query}{padding-left:0;}
+.p-none${query}{padding:0}
+.pb-none${query}{padding-block:0}
+.pbs-none${query}{padding-block-start:0}
+.pbe-none${query}{padding-block-end:0}
+.pi-none${query}{padding-inline:0}
+.pis-none${query}{padding-inline-start:0}
+.pie-none${query}{padding-inline-end:0}
 `
   function template (label, step, side, value) {
     side = side ? side = `-${side}` : ''
