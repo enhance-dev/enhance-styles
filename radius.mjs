e5a69bd294e75eeca6edf2d7d0c80f458e60025e
Cole Peters <cole@colepeters.com>
2023-04-26 14:00:56 -0500

    Update logical properties for border/radius

--- radius.mjs
+++ radius.mjs
@@ -6,18 +6,18 @@ export default function radius (state={}) {
 .radius-none${query}{border-radius:0;}
 .radius-100${query}{border-radius:100%;}
 .radius-pill${query}{border-radius:9999px;}
-.radius-t-none,
-.radius-r-none,
-.radius-tr-none${query}{border-top-right-radius:0;}
-.radius-b-none,
-.radius-r-none,
-.radius-br-none${query}{border-bottom-right-radius:0;}
-.radius-t-none,
-.radius-l-none,
-.radius-tl-none${query}{border-top-left-radius:0;}
-.radius-b-none,
-.radius-l-none,
-.radius-bl-none${query}{border-bottom-left-radius:0;}
+.radius-bs-none${query},
+.radius-is-none${query},
+.radius-ss-none${query}{border-start-start-radius:0;}
+.radius-bs-none${query},
+.radius-ie-none${query},
+.radius-se-none${query}{border-start-end-radius:0;}
+.radius-be-none${query},
+.radius-is-none${query},
+.radius-es-none${query}{border-end-start-radius:0;}
+.radius-be-none${query},
+.radius-ie-none${query},
+.radius-ee-none${query}{border-end-end-radius:0;}
 `
 
   radii.map(function (r, i) {
