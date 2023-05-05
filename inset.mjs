11f92a754e1e989fccb14b24b90c16d6fe27d740
Cole Peters <cole@colepeters.com>
2023-05-05 16:36:24 -0500

    Update inset to use sides

--- inset.mjs
+++ inset.mjs
@@ -1,12 +1,15 @@
+import sides from './sides.mjs'
+
 export default function inset (query='') {
-  return /*css*/`
+  let output = `
 /*** Inset ***/
 .inset-0${query}{inset:0}
-.inset-b-0${query}{inset-block:0}
-.inset-bs-0${query}{inset-block-start:0}
-.inset-be-0${query}{inset-block-end:0}
-.inset-i-0${query}{inset-inline:0}
-.inset-is-0${query}{inset-inline-start:0}
-.inset-ie-0${query}{inset-inline-end:0}
 `
+  const sideKeys = Object.keys(sides)
+
+  sideKeys.forEach(sideKey => {
+    output += `.inset-${sideKey}-0${query}{inset-${sides[sideKey]}:0;}\n`
+  })
+
+  return output
 }
