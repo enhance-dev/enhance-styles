88f7c0e508bc84fa6913696772cfa7c7fd49d57e
Cole Peters <cole@colepeters.com>
2023-05-05 16:42:48 -0500

    Set body font size

--- typeface.mjs
+++ typeface.mjs
@@ -2,11 +2,16 @@ import family from './family.mjs'
 
 export default function typeface(state={}) {
   const { config={} } = state
+  const bodyFontSize = config.typeScale
+    ? 'var(--text-0)'
+    : '1rem'
+
   return /*css*/`
 /*** Typeface ***/
 html {font-size: 16px;}
 ${family(config)}
 body {
+  font-size: ${bodyFontSize};
   font-weight: normal;
   line-height: 1;
   text-rendering: optimizeSpeed;
