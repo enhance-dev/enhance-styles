e5a10acadcc8a5d6255ed31cf5d9cfef3d90bd95
kj <kj@begin.com>
2023-06-01 16:31:44 -0700

    Removes root declaration if no custom properties present.

--- properties.mjs
+++ properties.mjs
@@ -2,7 +2,7 @@ export default function properties(state={}) {
   const { config } = state
   const { properties={} } = config
   let output = ''
-  if (Object.keys(properties)) {
+  if (Object.keys(properties).length) {
     output = /*css*/`
 /*** Custom Properties ***/
 :root {
