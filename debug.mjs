0c0754ca31adaaf64589a7dd867cfda22efd6dac
kj <kj@begin.com>
2023-06-29 16:23:06 -0700

    Simplifies debug classes

--- debug.mjs
+++ debug.mjs
@@ -1,21 +1,7 @@
 export default function Debug() {
   return `
 /*** Debug ***/
-.debug * { outline: 2px dotted var(--debug-color, darkviolet) }
-.debug * * { outline: 2px dotted var(--debug-color, darkviolet) }
-.debug * * * { outline: 2px dotted var(--debug-color, darkviolet) }
-.debug * * * * { outline: 2px dotted var(--debug-color, darkviolet) }
-.debug * * * * * { outline: 1px solid var(--debug-color, darkviolet) }
-.debug * * * * * * { outline: 1px solid var(--debug-color, darkviolet) }
-.debug * * * * * * * { outline: 1px solid var(--debug-color, darkviolet) }
-.debug * * * * * * * * { outline: 1px solid var(--debug-color, darkviolet) }
-.debug * *:hover { border:2px solid var(--debug-color, darkviolet) }
-.debug * * *:hover { border:2px solid var(--debug-color, darkviolet) }
-.debug * * * *:hover { border:2px solid var(--debug-color, darkviolet) }
-.debug * * * * *:hover { border:2px solid var(--debug-color, darkviolet) }
-.debug * * * * * *:hover { border:2px dotted var(--debug-color, darkviolet) }
-.debug * * * * * * *:hover { border:2px dotted var(--debug-color, darkviolet) }
-.debug * * * * * * * *:hover { border:2px dotted var(--debug-color, darkviolet) }
-.debug * * * * * * * * *:hover { border:2px dotted var(--debug-color, darkviolet) }
+.debug * { outline: 2px dotted var(--debug-color, rebeccapurple) }
+.debug *:hover { border:2px solid var(--debug-color, rebeccapurple) }
   `
 }
