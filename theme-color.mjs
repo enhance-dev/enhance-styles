44b69adfd3b1b551ad078211401da197e8c8fd4f
kj <kj@begin.com>
2023-09-26 17:17:55 -0700

    Adds minimal light / dark theme support with accent color

--- theme-color.mjs
+++ theme-color.mjs
@@ -2,7 +2,15 @@ import hextohsl from './hex-to-hsl.mjs'
 
 export default function themeColor({ config }) {
   const { color={}, theme={} } = config
-  theme['grey'] = theme && theme.grey || '#777'
+  const defaultLight = '#f8f9fa'
+  const defaultDark = '#343a40'
+  const lightParts = hextohsl(theme['light'] || defaultLight);
+  const darkParts = hextohsl(theme['dark'] || defaultDark)
+  const accentColor = theme['accent'] || '#663399'
+  const accentContrastColor = theme['accent-contrast'] || defaultDark
+  theme['accent'] = theme['accent'] || accentColor
+  theme['accent-contrast'] = theme['accent-contrast'] || accentContrastColor
+
 
   function colorSteps(color, name) {
     const hue = color.h
@@ -22,19 +30,49 @@ export default function themeColor({ config }) {
     `
   }
 
+
   return /*css*/`
 /*** Theme Colors ***/
 :root {
 ${Object.keys(theme).map(name => {
-if (name === 'light' || name === 'dark') {
+  if (name === 'light' ||
+      name === 'dark' ||
+      name === 'accent' ||
+      name === 'accent-contrast') {
   return `  --${name}: ${theme[name]};`
 }
-
 else {
   return colorSteps(hextohsl(theme[name]), name)
 }
 }).join('\n')}
+
 ${Object.keys(color).map(name => `  --${name}: ${color[name]};`).join('\n')}
+  --back: var(--light, ${defaultLight});
+  --fore: var(--dark, ${defaultDark});
+  ${colorSteps({ h: lightParts.h, s: 0, l: 50}, 'grey')}
+  --border-width: 1px;
+  --focus-size: 1px;
+  --focus-offset: 2px;
+  accent-color: var(--accent);
+  color-scheme: light dark;
+}
+
+:is(a, button, input, textarea, summary):focus:not(:focus-visible) {
+  outline: none;
+}
+
+:is(a, button, input, textarea, summary):focus-visible {
+  outline: max(var(--focus-size), 1px) solid var(--accent);
+  outline-offset: var(--border-width);
+  box-shadow: 0 0 0 1px black,
+    0 0 0 3px white;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --back: var(--dark, ${defaultDark});
+    --fore: var(--light, ${defaultLight});
+  }
 }
 `
 
