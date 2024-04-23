c6c0fabed30a95313f2763b865e3c2e7eed81131
Cole Peters <cole@colepeters.com>
2024-04-23 14:50:40 -0500

    Add class for screenreader only content

--- visibility.mjs
+++ visibility.mjs
@@ -1,7 +1,8 @@
-export default function overflow (query='') {
+export default function overflow(query = '') {
   return /*css*/`
 /*** Visibility ***/
 .invisible${query}{visibility:hidden;}
 .visible${query}{visibility:visible;}
+.screenreader-only${query}{border: 0; clip-path: inset(50%); height: 1px; margin: -1px; overflow: hidden; padding: 0; position: absolute; width: 1px; word-wrap: normal;}
 `
 }
